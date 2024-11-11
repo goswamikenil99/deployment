e500153de6a8fbd23681310a8523705d8b25f768
goswamikenil99 <kenilgirigoswami21@gnu.ac.in>
2024-11-11 22:27:38 +0530

    Initial commit

---
+++ Backend/routes/AuthRoutes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import {
+  getUserInfo,
+  login,
+  signup,
+  logout,
+  updateProfile,
+  addProfileImage,
+  removeProfileImage,
+  otp,
+} from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import multer from "multer";
+
+const authRoutes = Router();
+const upload = multer({ dest: "uploads/profiles/" });
+
+authRoutes.post("/signup", signup);
+authRoutes.post("/login", login);
+authRoutes.post("/logout", logout);
+authRoutes.post("/otp", otp);
+authRoutes.get("/userinfo", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
+authRoutes.post(
+  "/add-profile-image",
+  verifyToken,
+  upload.single("profile-image"),
+  addProfileImage
+);
+authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage);
+
+export default authRoutes;
